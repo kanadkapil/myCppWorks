814ba81f3c305fd2e729c209e6496db062c1b5f2
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2024-06-11 01:35:19 +0530

    Create nBase.cpp

---
+++ nBase.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int bin = 0b0000111; // binary
+    cout << bin << endl;
+
+    int hex = 0xab; //hexa
+    cout << hex << endl;
+
+    int oct = 012; // Octal number
+    cout << oct << endl;
+    
+    return 0;
+}
