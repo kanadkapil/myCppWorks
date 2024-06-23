73b4ef91480ab2bf2b4549af9e9feec4f09597d1
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2024-06-23 22:13:42 +0530

    Add files via upload

---
+++ nModulusNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter a number: ";
+    cin >> a;
+
+    if (a < 0)
+        cout << a * (-1);
+    else
+    cout << a;
+    }
