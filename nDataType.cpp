8f84ce0f6033d72c35a2ca3a818fa81c5064fdb9
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2024-06-23 22:14:04 +0530

    Add files via upload

---
+++ nDataType.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout << "size of int a " << sizeof(a) << endl;
+
+    float b;
+    cout << "size of float b " << sizeof(b) << endl; 
+
+    char c;
+    cout << "size of char c " << sizeof(c) << endl;
+
+    bool d;
+    cout << "size of bool d " << sizeof(d) << endl;
+}
