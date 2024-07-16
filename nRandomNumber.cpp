ef8665004207b005d4df7453426f030424c79ffa
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2024-07-16 12:17:03 +0530

    Update nRandomNumber.cpp

--- nRandomNumber.cpp
+++ nRandomNumber.cpp
@@ -8,8 +8,5 @@ int main(){
         int randomNumber = rand()%10;
         cout << "Random number: " << randomNumber << endl;
     }
-        for(int i=1; i <=5; i++){
-        int randomNumber = rand()%10;
-        cout <<randomNumber;
-    }
+
 }
