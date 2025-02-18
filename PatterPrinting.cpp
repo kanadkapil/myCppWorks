8cebb94ca0d3b7a78af795f3e9c64f9933aac9aa
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2025-02-18 21:51:01 +0530

    Add files via upload

---
+++ PatterPrinting.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //Print the pattern 
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    // string a = " * * * * *";
+    for (int i=0; i<=4; i++){
+        // cout << a << endl;
+        for (int j=0; j<=i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+}
