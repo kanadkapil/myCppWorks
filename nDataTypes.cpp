aeb289aa376ae98cef688cf7ae36ca7e147ef6f4
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2025-02-20 01:18:49 +0530

    Update nDataTypes.cpp

--- nDataTypes.cpp
+++ nDataTypes.cpp
@@ -3,12 +3,22 @@ using namespace std;
 
 int main(){
     int age = 25;
-    age =30;
+    age = 30;
     float weight = 65.6;
     double balance = 126325123567;
-    string user ="tup";
+    string user = "tup";
     char gender = 'm';
     bool isSunny = false;
-    string colors[]={"red","blue","green"};
-    cout << colors[1];
+    string colors[] = {"red", "blue", "green"};
+    
+    // Printing values of the variables
+    cout << "User: " << user << endl;
+    cout << "Age: " << age << endl;
+    cout << "Weight: " << weight << endl;
+    cout << "Balance: " << balance << endl;
+    cout << "Gender: " << gender << endl;
+    cout << "Is it sunny? " << (isSunny ? "Yes" : "No") << endl;
+    cout << "Favorite color: " << colors[1] << endl;  // "blue" from colors array
+
+    return 0;
 }
