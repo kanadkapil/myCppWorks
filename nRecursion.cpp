1ee3491911f956b8e687e071a26c278b9433f496
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2025-03-11 23:26:17 +0530

    Add files via upload

--- nRecursion.cpp
+++ nRecursion.cpp
@@ -1,27 +1,17 @@
-#include <iostream>
-#include <stdlib.h>
-#include <conio.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int fact(int n)
-{
-    if (n == 1)
-    {
-        return 1;
+void count(int n){
+    if (n == 0){
+        return;
     }
-    else
-    {
-        return n * fact(n - 1);
-    }
+    count(n-1);
+    cout << n << endl;
 }
 
-int main()
-{
 
-    int x, factorial;
-    cout << "Enter Digit: ";
-    cin >> x;
-
-    factorial = fact(x);
-    cout << factorial;
+int main(){
+    int n =10;
+    count (n);
 }
+
