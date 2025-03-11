28e1b8cdd7380f285e410e4109db07b30a232886
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2025-03-11 23:28:39 +0530

    Add files via upload

---
+++ nBinaryTreeInOrder.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right, *left;
+    Node (int d){
+        data = d;
+        right = NULL;
+        left =NULL;
+    }
+};
+
+
+void LDR(Node *root){ // inoder LDR
+    if(root == NULL) return;
+
+    LDR (root -> left);
+    cout << root -> data << " ";
+    LDR (root -> right);
+
+    
+    
+
+}
+
+int main(){
+    Node *r = new Node(1);
+
+    Node *rl = new Node(2);
+    Node *rr = new Node(3);
+    Node *rll = new Node(4);
+
+    Node *rlr = new Node(5);
+    Node *rrl = new Node(6);
+    Node *rrr = new Node(7);
+
+    r -> left = rl;
+    r -> right = rr;
+    rl -> left = rll;
+    rl -> right = rlr;
+    rr -> left = rrl;
+    rr -> right = rrr;
+
+    LDR(r);
+
+
+}
