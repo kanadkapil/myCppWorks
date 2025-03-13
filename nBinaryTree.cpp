9135f62f9e71df5601d1e63177c4a2e05dd2ddaf
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2025-03-13 12:08:49 +0530

    Update nBinaryTree.cpp

--- nBinaryTree.cpp
+++ nBinaryTree.cpp
@@ -11,15 +11,32 @@ struct Node {
     }
 };
 
+void levelOrder(Node *root) { // traversal
+    if (root == NULL) return;
 
+    queue<Node *> q; // queue to store nodes for BFS
+    q.push(root);
+
+    while (!q.empty()) {
+        Node *current = q.front();
+        q.pop();
+        
+        cout << current->data << " ";  // print the node's data
+        
+        // Push left and right children to the queue if they exist
+        if (current->left) q.push(current->left);
+        if (current->right) q.push(current->right);
+    }
+
+}
 
 int main(){
     Node *r = new Node(1);
 
     Node *rl = new Node(2);
     Node *rr = new Node(3);
-    Node *rll = new Node(4);
 
+    Node *rll = new Node(4);
     Node *rlr = new Node(5);
     Node *rrl = new Node(6);
     Node *rrr = new Node(7);
@@ -31,5 +48,6 @@ int main(){
     rr -> left = rrl;
     rr -> right = rrr;
 
+    levelOrder(r);
 
 }
